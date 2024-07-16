7d2fd6edcc5e779cd9d9db80e0120e9135b94948
Taykata <altaytva@gmail.com>
2024-07-16 16:13:39 +0300

    Modify server data

--- client/src/components/MainAppliances/AllAppliances/AllAppliances.jsx
+++ client/src/components/MainAppliances/AllAppliances/AllAppliances.jsx
@@ -1,8 +1,5 @@
 import { useState, useEffect } from 'react';
 import style from './AllAppliances.module.css';
-import washingMachine from '/images/washing-machine.jpg';
-import oven from '/images/oven.jpg';
-import dishwasher from '/images/dishwasher.jpg';
 import ApplianceDetails from '../ApplianceDetails/ApplianceDetails';
 import * as applianceService from '../../../services/applianceService';
 
@@ -11,8 +8,8 @@ export default function AllAppliances() {
     const [isModalOpen, setModalOpen] = useState(false);
     const [currentProduct, setCurrentProduct] = useState(null);
 
-    const createModal = (product) => {
-        setCurrentProduct(product);
+    const createModal = (appliance) => {
+        setCurrentProduct(appliance);
         setModalOpen(true);
     };
 
@@ -20,12 +17,6 @@ export default function AllAppliances() {
         setModalOpen(false);
         setCurrentProduct(null);
     };
-
-    const products = [
-        { title: 'Washing Machine', price: '$549.99', image: washingMachine, brand: 'Bosch', description: 'Introducing the Bosch Washing Machine, designed to effortlessly handle your laundry needs with precision and ease. Built for efficiency and reliability, this washing machine is the perfect blend of advanced technology and user-friendly features.' },
-        { title: 'Oven', price: '$244.99', image: oven, brand: 'Beko', description: 'Transform your kitchen into a culinary haven with the MasterChef 5000 Electric Convection Oven. Perfect for aspiring chefs and seasoned cooks alike, this oven combines cutting-edge technology with intuitive design to elevate your cooking experience.' },
-        { title: 'Dishwasher', price: '$269.99', image: dishwasher, brand: 'Miele', description: 'Simplify your kitchen cleanup with the SparkleClean 3000 Series Dishwasher, a powerhouse appliance designed to handle your dishes with efficiency and style. Engineered with advanced technology and user-friendly features, this dishwasher promises to revolutionize the way you clean.' }
-    ];
 
     useEffect(() => {
         applianceService.getAll()
@@ -50,4 +41,3 @@ export default function AllAppliances() {
         </>
     );
 }
-

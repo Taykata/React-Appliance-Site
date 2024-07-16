d845d2ae4d4bd3094afbe3743fffcb1cf21e5182
Taykata <altaytva@gmail.com>
2024-07-16 15:52:36 +0300

    Modify requester

--- client/src/services/applianceService.js
+++ client/src/services/applianceService.js
@@ -1,23 +1,15 @@
-import { request } from "../lib/request";
+import * as request from "../lib/request";
 
 const baseUrl = 'http://localhost:3030/jsonstore/appliances';
 
 export const getAll = async () => {
-    const result = await request('GET', baseUrl);
+    const result = await request.get(baseUrl);
 
     return Object.values(result);
 }
 
 export const create = async (applianceData) => {
-    const response = await fetch(baseUrl, {
-        method: 'POST',
-        headers: {
-            'content-type': 'application/json'
-        },
-        body: JSON.stringify(applianceData)
-    })
-
-    const result = await response.json();
+    const result = await request.post(baseUrl, applianceData);
 
     return result;
 }

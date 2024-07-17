94965c9e1ac13d8b2f53ad7ed1850f63606f81d5
Taykata <altaytva@gmail.com>
2024-07-17 22:12:16 +0300

    Add user register

--- client/src/services/authService.js
+++ client/src/services/authService.js
@@ -4,7 +4,17 @@ const baseUrl = 'http://localhost:3030/users';
 
 export const login = async (email, password) => {
     const result = await request.post(`${baseUrl}/login`, {
-        email, password
+        email,
+        password
+    });
+
+    return result;
+}
+
+export const register = async (email, password) => {
+    const result = await request.post(`${baseUrl}/register`, {
+        email,
+        password
     });
 
     return result;
